bbd2c2dde90a957f23417070f715389f243fa811
Paul-Sebastian Manole <pmanole@volvocars.com>
2025-03-02 17:07:23 +0200

    fix: correct service name log annotation

--- src/server/ServerLive.ts
+++ src/server/ServerLive.ts
@@ -11,8 +11,29 @@ const HttpServer = BunHttpServer.layer({
   port,
 }).pipe(Layer.tap(() => Effect.logInfo(`Server listening on port ${port}`)))
 
+/**
+ * - Logs each request.
+ * - Adds a log span with an incremental unique ID for each request (`http.span.#`).
+ * - Adds log annotations: `http.status`, `http.method`, `http.url`.
+ *
+ * Example structured log event (with just the fields this logger adds):
+ * ```json
+ * {
+ *   annotations: {
+ *     "http.status": 404,
+ *     "http.method": "GET",
+ *     "http.url": "/v1/news/blt0da5a2d4812d7fd22?locale=de-de",
+ *   },
+ *   spans: {
+ *     "http.span.1": 319,
+ *   },
+ * }
+ * ```
+ */
+const RequestLoggerMiddleware = HttpMiddleware.logger
+
 /** Live HTTP Server serving {@link ApiLive} and a Swagger UI. */
-export const ServerLive = HttpApiBuilder.serve(HttpMiddleware.logger).pipe(
+export const ServerLive = HttpApiBuilder.serve(RequestLoggerMiddleware).pipe(
   // Swagger endpoint served at /docs path by default:
   Layer.provide(HttpApiSwagger.layer()),
   // The BFF main API layer:
@@ -28,7 +49,7 @@ export const ServerLive = HttpApiBuilder.serve(HttpMiddleware.logger).pipe(
   // Layer.provide(SomeEffectService.Default)
 
   // Depending on logging schema, this might be needed, but should probably look at a custom logger first:
-  Layer.annotateLogs("service", "news-and-communications-bff"),
+  Layer.annotateLogs("service.name", "news-and-communications-bff"),
 
   // Keep logging layer last so it's applied to all above layers, ensuring the correct logging
   // format. This is important because it clears the loggers collection of all loggers that other
